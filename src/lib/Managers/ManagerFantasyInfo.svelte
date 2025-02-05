9e1b4becaf7426bfe1d84b09f0f088af034a2909
tzward <tzward69@gmail.com>
2025-02-05 17:56:39 -0500

    Update ManagerFantasyInfo.svelte

--- src/lib/Managers/ManagerFantasyInfo.svelte
+++ src/lib/Managers/ManagerFantasyInfo.svelte
@@ -216,7 +216,7 @@
             </div>
             <div class="infoIcon playerIcon">
                 <img class="favoritePlayer" src="https://sleepercdn.com/content/nfl/players/{viewManager.favoritePlayer}.jpg" alt="favorite player"/>
-		{#if viewManager.name === "Kyle"}
+		{#if viewManager.name === "Tom Barth"}
                 <img class="favoritePlayer" src="https://sleepercdn.com/content/nba/players/{viewManager.favoritePlayer}.jpg" alt="favorite player"/>
     		{/if}
             </div>
