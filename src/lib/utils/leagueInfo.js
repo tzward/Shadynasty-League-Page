99c75a1a4615cab6069b203f6b672d929395e6ab
tzward <tzward69@gmail.com>
2025-03-11 16:50:44 -0400

    Update leagueInfo.js
    Update for new league year

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1126672589084028928"; // your league ID
+export const leagueID = "1205289422937919488"; // your league ID
 export const leagueName = "ShaDynasty 2025"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
