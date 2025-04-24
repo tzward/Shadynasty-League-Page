f6a816f255afe74d6ec5dc8094b811296ecbd361
tzward <tzward69@gmail.com>
2025-04-24 16:19:16 -0400

    Update +page.svelte

--- src/routes/playerDatabase/+page.svelte
+++ src/routes/playerDatabase/+page.svelte
@@ -1,1 +1,28 @@
+<script>
+	import LinearProgress from '@smui/linear-progress';
+    import {AllPlayers} from '$lib/components';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
+	export let data;
+	const {players, leagueTeamManagersData} = data;
+
+    onMount(() => {
+        if(!players.length) {
+            goto('/');
+        }
+    })
+</script>
+
+<style>
+	.main {
+		position: relative;
+		z-index: 1;
+	}
+    .loading {
+        display: block;
+        width: 85%;
+        max-width: 500px;
+        margin: 80px auto;
+    }
+</style>
