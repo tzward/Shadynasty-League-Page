05b7d92887cf3085bfe22d13c8a2dc5b1d329a14
tzward <tzward69@gmail.com>
2025-04-25 11:57:22 -0400

    Update Player.svelte

--- src/lib/playerDatabase/Player.svelte
+++ src/lib/playerDatabase/Player.svelte
@@ -1,1 +1,27 @@
+<script>
+	import LinearProgress from '@smui/linear-progress';
+    import {Manager} from '$lib/components';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
+	export let data;
+	const {managers, manager, managersInfo} = data;
+
+    onMount(() => {
+        if(!managers.length) goto('/');
+        if(manager < 0) goto("/managers");
+    })
+</script>
+
+<style>
+	.main {
+		position: relative;
+		z-index: 1;
+	}
+    .loading {
+        display: block;
+        width: 85%;
+        max-width: 500px;
+        margin: 80px auto;
+    }
+</style>
