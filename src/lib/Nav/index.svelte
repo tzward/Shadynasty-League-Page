90230a1113cf8811efc8959465177acdfb848bec
tzward <tzward69@gmail.com>
2025-04-25 19:02:52 -0400

    Update index.svelte

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -41,8 +41,8 @@
 		position: absolute;
 		display: block;
 		height: 5px;
-		width: 10%;
-		top: 20em;
+		width: 100%;
+		top: 20%;
 		background-color: #CD3498; /* #ff4ba8 */
 		box-shadow: 0 0 4px 4px #FDC2E1;
 	}
