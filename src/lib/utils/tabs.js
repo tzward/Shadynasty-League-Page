edaad041d3d988aa6f75a3c56a19ff4d04178d60
tzward <tzward69@gmail.com>
2025-04-28 12:18:46 -0400

    Update tabs.js
    fix tabs not switching to selected tab on the pagination bar.

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -5,26 +5,31 @@ export const tabs = [
         icon: 'home',
         label: 'Home',
         dest: '/',
+        key: 'home',
     },
     {
         icon: 'sports',
         label: 'Matchups',
         dest: '/matchups',
+        key: 'matchups',
     },
     {
         icon: 'swap_horiz',
         label: 'Trades & Waivers',
         dest: '/transactions',
+        key: 'transactions',
     },
     {
         icon: 'article',
         label: 'Blog',
         dest: '/blog',
+        key: 'blog',
     },
     {
         icon: 'view_comfy',
         label: 'League Info',
         nest: true,
+        key: 'league_info',
         children: [
             {
                 icon: 'storage',
@@ -77,10 +82,12 @@ export const tabs = [
         icon: 'person_search',
         label: 'Player Database',
         dest: '/playerDatabase',
+        key: 'playerDatabase',
     },
     {
         icon: 'lightbulb',
         label: 'Resources',
         dest: '/resources',
+        key: 'resources',
     },
 ];
