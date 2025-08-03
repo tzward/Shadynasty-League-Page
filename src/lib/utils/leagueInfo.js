eddd6606145bf960d22cbd9ed49cf7ea81110a20
tzward <tzward69@gmail.com>
2025-08-03 15:45:40 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -281,13 +281,13 @@ export const managers = [
        "tookOver": 2025, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Rutgas, NJ", // (optional)
        "bio": "1/2 of the NETJETS. Fake Jets fan",
-       "photo": "/managers/IMG_1048.jpeg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/IMG_1048.PNG", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "sea", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
-         name: "Irvin", // Can be anything (usually your rival's name)
-         link: 6, // manager array number within this array, or null to link back to all managers page
+         name: "Nick", // Can be anything (usually your rival's name)
+         link: 3, // manager array number within this array, or null to link back to all managers page
          image: "/managers/File 24.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
        "favoritePlayer": 9488, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
@@ -306,7 +306,7 @@ export const managers = [
        "tookOver": 2025, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Rutgas, NJ", // (optional)
        "bio": "1/2 of the NETJETS. G5 lover, go tell your mother.",
-       "photo": "/managers/IMG_2490.jpeg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/IMG_2490.PNG", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
