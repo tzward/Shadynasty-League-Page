3de807f10d96529788c33b5f88d5738f0acb4065
tzward <tzward69@gmail.com>
2025-08-03 15:48:43 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -281,7 +281,7 @@ export const managers = [
        "tookOver": 2025, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Rutgas, NJ", // (optional)
        "bio": "1/2 of the NETJETS. Fake Jets fan",
-       "photo": "/managers/IMG_1048.PNG", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/Glenn.avif", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "sea", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -306,7 +306,7 @@ export const managers = [
        "tookOver": 2025, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Rutgas, NJ", // (optional)
        "bio": "1/2 of the NETJETS. G5 lover, go tell your mother.",
-       "photo": "/managers/IMG_2490.PNG", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/Woody.avif", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
