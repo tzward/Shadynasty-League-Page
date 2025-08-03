4e89ed37c56441611a90ac5c9afb9c5072b5b198
tzward <tzward69@gmail.com>
2025-08-03 15:39:49 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -306,7 +306,7 @@ export const managers = [
        "tookOver": 2025, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Rutgas, NJ", // (optional)
        "bio": "1/2 of the NETJETS. G5 lover, go tell your mother.",
-       "photo": "/managers/IMG_2490.JPEG", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/IMG_2490.jpeg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
